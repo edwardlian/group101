a0f7020d1906c023a0c1dfab7ba6238addfe8607
edwardlian <edward16005@gmail.com>
2015-05-17 01:36:32 +0800

    Install Devise

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'bootstrap-sass'
 gem 'simple_form'
 gem 'tzinfo-data'
+gem 'devise', '3.4.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :find_group
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
 
   def new
     @post = @group.posts.new
