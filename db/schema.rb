671550346203e38005c451cf43930a68998cf71b
edwardlian <edward16005@gmail.com>
2015-05-19 23:20:48 +0800

    Implement Simple Backstage

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518143521) do
+ActiveRecord::Schema.define(version: 20150519145907) do
 
   create_table "group_users", force: :cascade do |t|
     t.integer  "group_id",   limit: 4
@@ -23,9 +23,10 @@ ActiveRecord::Schema.define(version: 20150518143521) do
   create_table "groups", force: :cascade do |t|
     t.string   "title",       limit: 255
     t.text     "description", limit: 65535
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.integer  "user_id",     limit: 4
+    t.integer  "posts_count", limit: 4,     default: 0
   end
 
   create_table "posts", force: :cascade do |t|
