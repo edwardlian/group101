fa3a01fcc6f6f133a452fb8cf13e76b51810127d
edwardlian <edward16005@gmail.com>
2015-05-21 23:39:19 +0800

    Tutorial End

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Create Dummy Data!"
+puts "Create Account*1, Group*10, Post*20"
+
+create_account = User.create [email: "example@gmail.com", password: "12345678", password_confirmation: "12345678"]
+
+create_groups = for i in 1..10 do
+  Group.create! [title: "Group No.#{i}", description: "種子的第 #{i} 個討論版", user_id: "1"]
+
+  for j in 1..20 do
+    Post.create! [group_id: "#{i}", content: "種子的第 #{j} 個留言", user_id: "1"]
+  end
+end
